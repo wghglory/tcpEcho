481753e1ac863ec35c79d3fa14fdf6c504217d14
Guanghui Wang <wghglory89@gmail.com>
2017-03-18 16:35:46 -0700

    * .gitignore: tcp echo server
    * TcpEcho.sln:
    * Program.cs:
    * Program.cs:
    * TcpEchoClient.csproj:
    * TcpEchoServer.csproj:
    * AssemblyInfo.cs:
    * AssemblyInfo.cs:

---
+++ TcpEchoClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpEchoClient
+{
+	class TcpEchoClient
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Starting echo client...");
+
+			int port = 1234;
+
+			TcpClient client = new TcpClient("localhost", port);
+			NetworkStream stream = client.GetStream();
+			StreamReader reader = new StreamReader(stream);
+			StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+
+			while (true)
+			{
+				Console.Write("Enter text to send: ");
+				string lineToSend = Console.ReadLine();
+				Console.WriteLine("Sending to server: " + lineToSend);
+				writer.WriteLine(lineToSend);
+				string lineReceived = reader.ReadLine();
+				Console.WriteLine("Received from server: " + lineReceived);
+			}
+		}
+
+	}
+}
---
+++ TcpEchoServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpEchoServer
+{
+	public class TcpEchoServer
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Starting echo server...");
+
+			int port = 1234;
+			TcpListener listener = new TcpListener(IPAddress.Loopback, port);
+			listener.Start();
+
+			TcpClient client = listener.AcceptTcpClient();
+			NetworkStream stream = client.GetStream();
+			StreamWriter writer = new StreamWriter(stream, Encoding.ASCII) { AutoFlush = true };
+			StreamReader reader = new StreamReader(stream, Encoding.ASCII);
+
+			while (true)
+			{
+				string inputLine = "";
+				while (inputLine != null)
+				{
+					inputLine = reader.ReadLine();
+					writer.WriteLine("Echoing string: " + inputLine);
+					Console.WriteLine("Echoing string: " + inputLine);
+				}
+				Console.WriteLine("Server saw disconnect from client.");
+			}
+		}
+	}
+}
